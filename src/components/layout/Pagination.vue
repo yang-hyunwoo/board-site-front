a3eb25740b20b1b58860e5f8c48d5fefadc00bd7
cohouseol <cohouseol@gmail.com>
2022-08-12 00:44:31 +0900

    여행 리스트 페이지 완료
    여행 리스트 페이지를 완료 했다.
    현재 페이지네이션이 조금 이상해서 이 부분만 수정 하면 될거 같다.

---
+++ src/components/layout/Pagination.vue
@@ -0,0 +1,61 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<div class="pagination" >
+    <b-card style="justify-content: center;">
+      <div class="overflow-auto">
+        <div class="mt-3">
+          <b-pagination v-model="ex4CurrentPage" pills :total-rows="ex4Rows" @page-click="pageClick"></b-pagination>
+        </div>
+      </div>
+    </b-card>
+  <slot/>
+</div>
+</template>
+
+
+<style>
+
+.card {
+  --bs-card-border-width:0px !important
+}
+
+.pagination {
+  justify-content : center;
+}
+
+</style>
+
+<script>
+export default {
+	data: function () {
+    return {
+    ex4CurrentPage:1,
+    ex4Rows:200
+    }
+  },
+  methods: {
+    pageClick: function (button, page){ 
+         this.ex4CurrentPage = page;
+      // this.getNoticeListByPage(page);
+			},
+			// getNoticeListByPage: function (page) {
+      //           var limit = 20;
+      //           var offset = (page - 1) * limit;
+
+      //           this.loading = true;
+      //           this.$http
+      //               .get('/notice/list' + `?offset=${offset}&limit=${limit}`)
+      //               .then(response => {
+      //                   this.items = response.data;
+      //               })
+      //               .catch(error => {
+      //                   console.log(error);
+      //               })
+      //               .finally(() => this.loading = false);
+			// }
+
+  }
+
+}
+
+</script>
