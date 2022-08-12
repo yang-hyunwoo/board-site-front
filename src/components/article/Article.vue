76575bb48a0a4c840bfccd39421b5af7b61bc7ed
cohouseol <cohouseol@gmail.com>
2022-08-12 22:19:49 +0900

    editor 추가
    에디터 추가를 완료 했다.

---
+++ src/components/article/Article.vue
@@ -0,0 +1,145 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+ <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <p class="lead text-muted">오늘의 여행 어쩌구</p>
+        <p>
+            <input type="text" placeholder="검색어를 입력하세요."/>
+        </p>
+      </div>
+    </div>
+
+
+<b-table-simple hover small caption-top responsive>
+
+  <colgroup>
+    <col />
+    <col />
+  </colgroup>
+  <colgroup>
+    <col />
+    <col />
+    <col />
+  </colgroup>
+  <colgroup>
+    <col />
+  </colgroup>
+  <b-thead head-variant="dark">
+    <b-tr>
+      <b-th>제목</b-th>
+      <b-th>내용</b-th>
+      <b-th>여행사</b-th>
+      <b-th>여행사 여행 이름</b-th>
+      <b-th>작성자</b-th>
+      <b-th>작성일</b-th>
+    </b-tr>
+  </b-thead>
+  <b-tbody>
+    <b-tr @click="articleDetailClick">
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+   <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+     <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+     <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+     <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+     <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+         <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+         <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+         <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+    <b-tr>
+      <b-th>Belgium</b-th>
+      <b-th class="text-right">Antwerp</b-th>
+      <b-td>56</b-td>
+      <b-td>22</b-td>
+      <b-td>43</b-td>
+      <b-td>23</b-td>
+    </b-tr>
+  </b-tbody>
+  <button @click="writer">글 쓰기</button>
+  <!-- <b-tfoot>
+    <b-tr>
+      <b-td colspan="7" variant="secondary" class="text-right"> Total Rows: <b>5</b> </b-td>
+    </b-tr>
+  </b-tfoot> -->
+</b-table-simple>
+  </section>
+</template>
+<script>
+export default {
+	data: function () {
+    return {
+    }
+  },
+  methods: {
+    articleDetailClick() {
+      this.$router.push("/articleDetail");
+    },
+    writer(){
+        this.$router.push("/articleWrite");
+    }
+  }
+
+}
+</script>
---
+++ src/components/article/ArticleDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <ToastViewer :articleId="articleId" :contentType="contentType" ></ToastViewer>
+        {{articleId}}
+<button @click="ssss">수정</button>
+<button @click="ssss2">삭제</button>
+</template>
+
+
+<script>
+
+ import ToastViewer from '@/components/editor/ToastViewer.vue'
+
+export default {
+	data: function () {
+    return {
+        articleId:0,
+        contentType:'',
+    }
+  },
+  components :{
+        ToastViewer
+  },
+  created(){
+    this.articleId=1;
+    this.contentType ="article"
+    console.log("111");
+  },    
+  methods: {
+
+  }
+
+}
+</script>
