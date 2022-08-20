cb1aa1eb4c00b772c98bc6e97da9719e805a4824
cohouseol <cohouseol@gmail.com>
2022-08-20 13:36:38 +0900

    samesite 오류 수정
    iamport 사용하기 위해 아임포트의 js를 받아 사용 하려고 했는데
    콘솔창에 samesite 오류가 발생했다.
    그래서 vue-cookie3를 install 받은 후
    main.js 에서 samesite를 설정 하였다.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -155,6 +155,7 @@
 
 <script>
 import login from "../assets/test.png"
+// import hideKey from 'dotenv';
 
 export default {
 	data: function () {
@@ -170,10 +171,38 @@ export default {
   },
   methods: {
     aaa() {
-         this.$axios.get("/api/trip/articles")
-      .then(res => {
-        console.log(res.data);
-      })
+      var IMP = window.IMP;
+      IMP.init(process.env.VUE_APP_IAMPORT);
+      console.log("111");
+
+
+    //  IMP.request_pay({ // param
+    //       pg: "html5_inicis",
+    //       pay_method: "card",
+    //       merchant_uid: "ORD20180131-0000011",
+    //       name: "노르웨이 회전 의자",
+    //       amount: 64900,
+    //       buyer_email: "gildong@gmail.com",
+    //       buyer_name: "홍길동",
+    //       buyer_tel: "010-4242-4242",
+    //       buyer_addr: "서울특별시 강남구 신사동",
+    //       buyer_postcode: "01181"
+    //   }, function (rsp) { // callback
+    //       if (rsp.success) {
+    //           console.log("111111111");
+    //           // 결제 성공 시 로직,
+    //       } else {
+    //           // 결제 실패 시 로직,
+    //       }
+    //   });
+
+
+
+
+      //    this.$axios.get("/api/trip/articles")
+      // .then(res => {
+      //   console.log(res.data);
+      // })
     }
 
   }
--- src/main.js
+++ src/main.js
@@ -6,11 +6,12 @@ import router from './router/router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import axios from "axios"; 
-
+import { useCookies } from "vue3-cookies";
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios; 
 app.use(BootstrapVue3)
 app.use(mitt)
+app.use((useCookies({ sameSite: "none", secure: true })))
 app.use(router)
 app.mount('#app')
